c6e894ea71c7d8e1681087770a580ded4676385e
ms.moradi <ms.moradi47@gmail.com>
2024-12-09 20:21:55 +0330

    Writing the click function to close the icon

--- Js/main.js
+++ Js/main.js
@@ -2,10 +2,20 @@ let $ = document;
 
 let btnElem = $.querySelector('.btn');
 let modalElem = $.querySelector('.modal');
-console.log( modalElem)
+let divElem = $.querySelector('.container');
+let iElem = $.querySelector('#close');
+console.log( iElem)
 
 btnElem.addEventListener('click', function(){
 
     modalElem.style.display = 'flex';
+    divElem.style.filter = 'Blur(5px)';
     console.log('open')
+})
+
+iElem.addEventListener('click', function(){
+
+    modalElem.style.display = 'none';
+    divElem.style.filter = 'Blur(0px)';
+
 })
